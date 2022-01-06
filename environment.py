df59008b133bc02ffc88a0cc8b89e46e3ed0acde
Mariano Alvez <alvez_jmariano@hotmail.com>
2022-01-06 20:28:31 -0300

    Metodo para enviar email ok Funcionando. | Sendgrid configurado

---
+++ environment.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  6 18:59:02 2022
+
+@author: User
+"""
+
+import os
--- servidor.py
+++ servidor.py
@@ -6,12 +6,40 @@ Created on Thu Jan  6 10:58:27 2022
 """
 
 from flask import Flask
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/correo")
+def enviarCorreo():
+    destino = request.args.get("destino")
+    asunto  = request.args.get("asunto")
+    mensaje = request.args.get("mensaje")
+    hashString = request.args.get("hash")
+    if(hashString == os.environ.get("SECURITY_HASH")):
+        message = Mail(
+            from_email=os.environ.get("from_email"),
+            to_emails = destino,
+            subject = asunto,
+            html_content= mensaje )
+        try:
+            sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+            response = sg.send(message)
+            print("Enviado")
+            return "OK"
+        except Exception as e:
+            print(e.message)
+            return "KO"
+    else:
+        print("Hash error")
+        return "KO"
 
 if __name__ == "__main__":
     app.run()
